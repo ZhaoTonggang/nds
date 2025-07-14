caba2efa1f5d696dbcd99fb8c9d390c77c122bd1
root <root@cvm-3kqd4y52da223.novalocal>
2025-07-15 05:20:42 +0800

    update

--- js/index.js
+++ js/index.js
@@ -44,12 +44,13 @@ const intdata = () => {
 				let opgamev = data[j].v ? '(' + data[j].v + ')' : '';
 				let gbios = data[j].b ? '&b=' + data[j].b : '';
 				let grom = data[j].s ? '&s=' + data[j].s : '';
-				let purl = encodeURI('./play/?n=' + data[j].n + opgamev + gbios + '&i=' + data[j].i + grom);
-				item += '<a href="' + purl + '" title="' + data[j].n + opgamev +
-					'" target="_self"><div class="item">' +
-					'<div class="img_box"><img src="./imgs/' + data[j].i + '.png" alt="' + data[j].n + opgamev +
-					'">' +
-					span1 + span2 + '</div><p class="item_p1">' + data[j].n + '</p></div></a>';
+				let purl = './play/?' + btoa(encodeURI('n=' + data[j].n + opgamev + gbios + '&i=' +
+					data[j].i + grom)) + '.html';
+				item += '<a href="' + purl + '" title="' + data[j].n +
+					opgamev + '" target="_self"><div class="item">' +
+					'<div class="img_box"><img src="./imgs/' + data[j].i + '.png" alt="' + data[j].n +
+					opgamev + '">' + span1 + span2 + '</div><p class="item_p1">' + data[j].n +
+					'</p></div></a>';
 			};
 			app.innerHTML = item;
 		})
